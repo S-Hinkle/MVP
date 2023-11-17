9543d8acdff45e817734bcb1720fab464d6b4653
S-Hinkle <shcatchall@protonmail.com>
2023-11-17 18:39:24 -0500

    added market page and chart for market data

--- public/main.js
+++ public/main.js
@@ -1,16 +1,213 @@
 
 
+// window.onload = () => {
+//     const contentContainer = document.getElementById('content-container');
+//     contentContainer.innerHTML = `
+//         <div class="welcome-page">
+//             <h1>Welcome to Our Website</h1>
+//             <p>Some introductory text or instructions.</p>
+//             <button id="enter-site">Enter Site</button>
+//         </div>
+//     `;
+// };
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementById('enter-site').addEventListener('click', () => {
+//         loadContent('Market'); // Assuming 'Home' is your default view
+//     });
+// });
+
+
+
+
+
+function loadMarketContent() {
+    const contentContainer = document.getElementById('content-container');
+    contentContainer.innerHTML = `
+        <div class="market-container">
+            <div class="box-row">
+                <!-- Column 1 -->
+                <div class="column">
+                    <div class="box" id="marketCap"></div>
+                    <div class="box" id="marketVolume"></div>
+                </div>
+
+                <!-- Column 2 -->
+                <div class="column">
+                    <div class="box" id="marketPercentChange"></div>
+                    <div class="box" id="ongoingICOs"></div>
+                </div>
+
+                <!-- Column 3 -->
+                <div class="column">
+                    <div class="box" id="coins"></div>
+                    <div class="box" id="markets"></div>
+                </div>
+
+                <!-- Canvas Column -->
+                <div class="column">
+                    <canvas class="box" id="marketCapChart"></canvas>
+                </div>
+            </div>
+            <div class="large-box"></div>
+        </div>
+    `;
+    getMarketData();
+}
+
+
+async function getMarketData() {
+
+    try {
+        const responseMarketData = await fetch('/api/market-data');
+        if (!responseMarketData.ok) {
+            throw new Error(`HTTP error! Status: ${responseMarketData.status}`);
+        }
+        const dataMarket = await responseMarketData.json();
+        //console.log(dataMarket); // TROUBLESHOOTING REMOVE LATER
+
+
+        const responseTopCoins = await fetch('/api/top-coins');
+        if (!responseTopCoins.ok) {
+            throw new Error(`HTTP error! Status: ${responseTopCoins.status}`);
+        }
+        const dataTopCoins = await responseTopCoins.json();
+        console.log(dataTopCoins); // TROUBLESHOOTING REMOVE LATER
+
+
+
+        addMarketData(dataMarket);
+
+    } catch (error) {
+        console.error('Error fetching market data:', error);
+    }
+}
+
+
+
+function addMarketData(data) {
+    const marketCap = document.querySelector("#marketCap");
+    const marketVolume = document.querySelector("#marketVolume");
+    const marketPercentChange = document.querySelector("#marketPercentChange");
+    const ongoingICOs = document.querySelector("#ongoingICOs");
+    const coins = document.querySelector("#coins");
+    const markets = document.querySelector("#markets");
+
+
+    marketCap.innerHTML = `Market Cap<br><br> $${data.data.total_market_cap.usd.toLocaleString()}`;
+    marketVolume.innerHTML = `24h Volume<br><br> $${data.data.total_volume.usd.toLocaleString()}`;
+    marketPercentChange.innerHTML = `Market Cap 24h Change<br><br> ${data.data.market_cap_change_percentage_24h_usd}%`;
+    ongoingICOs.innerHTML = `Ongoing ICOs<br><br> ${data.data.ongoing_icos}`;
+    coins.innerHTML = `Total CryptoCurrencies<br><br> ${data.data.active_cryptocurrencies}`;
+    markets.innerHTML = `Total Markets<br><br> ${data.data.markets}`;
+    
+
+    createMarketCapBreakdownChart(data.data.market_cap_percentage);
+}
+
+
+
+function createMarketCapBreakdownChart(dataObject) {
+    const myCanvas = document.getElementById("marketCapChart");
+    //console.log('Test canvas:', myCanvas); // TROUBLESHOOTING REMOVE LATER
+    const ctx = myCanvas.getContext("2d");
+
+
+    const labels = Object.keys(dataObject);
+    const dataPoints = Object.values(dataObject);
+    //console.log(labels); // TROUBLESHOOTING REMOVE LATER
+    //console.log(dataPoints); // TROUBLESHOOTING REMOVE LATER
+
+    const myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Market Cap Percentage',
+                data: dataPoints,
+                backgroundColor: [
+                    // You can specify different colors for each segment here
+                    '#8071DD', // BTC (Bitcoin)
+                    '#323540', // ETH (Ethereum)
+                    '#F4F3F6', // USDT (Tether)
+                    '#242631', // BNB (Binance Coin)
+                    '#F8F8F8', // XRP (Ripple)
+                    '#8071DD', // SOL (Solana)
+                    '#323540', // USDC (USD Coin)
+                    '#F4F3F6', // STETH (Lido Staked Ether)
+                    '#242631', // ADA (Cardano)
+                    '#F8F8F8'  // DOGE (Dogecoin)
+                ],
+                borderColor: [
+                    // Border colors for each segment
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: true,
+                    text: 'Market Cap Breakdown'
+                }
+            }
+        }
+    });
+}
+
+
+
+
+
+document.querySelectorAll('.navbar-links a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const page = this.textContent.trim();
+        loadContent(page);
+    });
+});
+
+
+
+function loadContent(page) {
+    const contentContainer = document.getElementById('content-container');
+    switch (page) {
+        case 'Portfolio':
+            contentContainer.innerHTML = '<p>Portfolio content goes here.</p>';
+            break;
+        case 'Coin Screener':
+            contentContainer.innerHTML = '<p>Coin Screener content goes here.</p>';
+            break;
+        case 'Market':
+            loadMarketContent();
+            break;
+        case 'Wallet Analysis':
+            contentContainer.innerHTML = '<p>Wallet Analysis content goes here.</p>';
+            break;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
 document.querySelector(".metamask-connect").addEventListener("click", login);
 
-
 async function login() {
     if (typeof window.ethereum !== 'undefined') {
         try {
             const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            console.log(accounts);
+            console.log(accounts); // TROUBLESHOOTING REMOVE LATER
             const account = accounts[0];
             fetch('http://localhost:3000/user/login', {
                 method: 'POST',
@@ -27,7 +224,7 @@ async function login() {
             })
             .then(data => {
                 // Handle the response data
-                console.log(data);
+                console.log(data); // TROUBLESHOOTING REMOVE LATER
             })
             .catch(error => {
                 console.error('Fetch error:', error);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import the necessary modules
 import express from 'express';
-
+import Moralis from 'moralis';
+import { EvmChain } from '@moralisweb3/common-evm-utils';
 import { promises as fs } from 'fs';
 import dotenv from "dotenv";
 import cors from 'cors';
@@ -33,6 +34,8 @@ app.use(express.static('public'));
 
 
 
+
+
 app.post('/user/login', async (req, res) => {
     const userAddress = req.body.account;
 
@@ -40,13 +43,13 @@ app.post('/user/login', async (req, res) => {
         // Check if the user exists
         const userQuery = 'SELECT * FROM wallets WHERE wallet_address = $1';
         const userResult = await pool.query(userQuery, [userAddress]);
-        console.log('userResult: ' + JSON.stringify(userResult.rows[0].id))
+        console.log('userResult: ' + JSON.stringify(userResult.rows[0].id)) // TROUBLESHOOTING REMOVE LATER
 
         if (userResult.rows.length === 0) {
             // User does not exist, create a new one
             const insertQuery = 'INSERT INTO wallets (wallet_address) VALUES ($1) RETURNING id';
             const insertResult = await pool.query(insertQuery, [userAddress]);
-            console.log('insertResult: ' + insertResult)
+            console.log('insertResult: ' + insertResult) // TROUBLESHOOTING REMOVE LATER
             // Return the new user data
             return res.json({ success: true, userId: insertResult.rows[0].id, newUser: true });
         } else {
@@ -58,6 +61,48 @@ app.post('/user/login', async (req, res) => {
         return res.status(500).json({ success: false, message: 'Internal server error' });
     }
 });
+
+
+
+
+
+// Route to fetch market data from CoinGecko
+app.get('/api/market-data', async (req, res) => {
+    try {
+        const url = `https://api.coingecko.com/api/v3/global?x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`;
+        const coingeckoResponse = await fetch(url);
+        
+        if (!coingeckoResponse.ok) {
+            throw new Error(`HTTP error! Status: ${coingeckoResponse.status}`);
+        }
+
+        const data = await coingeckoResponse.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching market data from CoinGecko:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+// Route to fetch top coin data from CoinGecko
+app.get('/api/top-coins', async (req, res) => {
+    try {
+        const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d%2C30d&locale=en&x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`;
+        const coingeckoResponse = await fetch(url);
+        
+        if (!coingeckoResponse.ok) {
+            throw new Error(`HTTP error! Status: ${coingeckoResponse.status}`);
+        }
+
+        const data = await coingeckoResponse.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching market data from CoinGecko:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 
 
